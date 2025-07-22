07dfa2aa20d9fd11a788d451b3ad0110d67f0a24
iain nash <me@iain.in>
2025-07-22 17:15:38 -0400

    github pages fix

--- zora-coin-creator/vite.config.ts
+++ zora-coin-creator/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sdk-demos/zora-coin-creator/',
+  base: '/zora-coins-sdk-demo/zora-coin-creator/',
 })
